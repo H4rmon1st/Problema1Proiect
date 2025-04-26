98dd36543b9e11abef21cb73e92efc357053a420
Jardan Alexandr <147718215+H4rmon1st@users.noreply.github.com>
2025-04-26 13:27:05 +0300

    Update Cow.cs

--- Cow.cs
+++ Cow.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace problema_1_proiect.Models
+namespace Problema1Proiect.Models
 {
-    internal class Cow
+    public class Cow : Animal
     {
+        public override string Type => "Cow";
+
+        public override string MakeSound()
+        {
+            return "Moo!";
+        }
     }
 }
