de7aea01d18322c64eb0413265917a75bea0ad8b
Jardan Alexandr <147718215+H4rmon1st@users.noreply.github.com>
2025-04-26 13:26:26 +0300

    Update Animal.cs

--- Animal.cs
+++ Animal.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace problema_1_proiect.Models
+namespace Problema1Proiect.Models
 {
-    internal class Animal
+    public abstract class Animal
     {
+        public abstract string Type { get; }
+        public abstract string MakeSound();
     }
 }
